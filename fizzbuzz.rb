9be8bf06f12ba52a4b92b1fab5586c5f541d8a6a
roxm <rossymr@gmail.com>
2015-12-29 15:44:39 -0600

    refactor making one function

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 class FizzBuzz
-  def is_divisible_by_3(number)
-		number % 3 == 0? true : false
-  end
-  
-   def is_divisible_by_5(number)
-   		number % 5 == 0? true : false
-   end
    
+   def is_divisible_by(divisor, number)
+   		number % divisor == 0? true : false
+   end
+
   
 end
   
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,22 +8,22 @@ end
 
 context 'Given a number, return TRUE because is divisible by' do
 	it 'three' do	
- 		expect(@fizzbuzzgame.is_divisible_by_3(3)).to be true
+ 		expect(@fizzbuzzgame.is_divisible_by(3,3)).to be true
  	end
  
  	it 'five' do	
- 		expect(@fizzbuzzgame.is_divisible_by_5(5)).to be true
- 	end
- 	
+ 		expect(@fizzbuzzgame.is_divisible_by(5,5)).to be true
+ 	end	
 end
 
 context 'Given a number, return FALSE because is NOT divisible by' do
 	it 'three, pass ONE then false' do	
- 		expect(@fizzbuzzgame.is_divisible_by_3(1)).to be false
+ 		expect(@fizzbuzzgame.is_divisible_by(3,1)).to be false
  	end
  	 it 'FIVE & pass SIX then false' do	
- 		expect(@fizzbuzzgame.is_divisible_by_5(6)).to be false
+ 		expect(@fizzbuzzgame.is_divisible_by(5,6)).to be false
  	end
 end
+
 
 end
