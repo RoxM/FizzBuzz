3e95f9f84fe86a1e3110075aa35c87cd01cc252d
roxm <rossymr@gmail.com>
2015-12-29 12:39:28 -0600

    divisible by 5 green

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,10 @@ class FizzBuzz
 		number % 3 == 0? true : false
   end
   
+   def is_divisible_by_5(number)
+   		number % 5 == 0? true : false
+   end
+  
 end
   
  
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,11 +7,17 @@ before do
 end
 
 context 'know if number is divisible by' do
-	it 'three, pass three' do	
+	it 'three, pass three then true' do	
  		expect(@fizzbuzzgame.is_divisible_by_3(3)).to be true
  	end
- 	it 'three, pass ONE' do	
+ 	it 'three, pass ONE then false' do	
  		expect(@fizzbuzzgame.is_divisible_by_3(1)).to be false
+ 	end
+ 	it 'five & pass five then true' do	
+ 		expect(@fizzbuzzgame.is_divisible_by_5(5)).to be true
+ 	end
+ 	 it 'FIVE & pass SIX then false' do	
+ 		expect(@fizzbuzzgame.is_divisible_by_5(6)).to be false
  	end
 end
 
